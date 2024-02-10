da002ec7e750fe9b57507c77c26bd3adb7e34579
muhammdiqball <iqbalqoriemail@gmail.com>
2024-02-11 03:14:29 +0700

    first commit

---
+++ public/js/script.js
@@ -0,0 +1,6 @@
+
+function hideart() {
+    document.getElementById("txtid").setAttribute("class", "d-none");
+
+    document.getElementById("txtid2").classList.remove("d-none");
+};
---
+++ resources/views/LandingPage.blade.php
@@ -0,0 +1,92 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <!-- Bootstrap CSS -->
+
+    <!-- Own CSS -->
+    <link rel="stylesheet" href="css/style.css">
+    <!-- Own CSS -->
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@500&display=swap" rel="stylesheet">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+   <link href="https://fonts.googleapis.com/css2?family=Poppins:ital@1&display=swap" rel="stylesheet">
+    <!-- Fonts -->
+
+    <link rel="icon" type="image/x-icon" href="favicon.ico">
+
+    <title>Landing Page</title>
+
+  </head>
+
+  <body>
+
+    <!-- Navbar -->
+    <nav class="navbar navbar-light mb-4">
+    <div class="container-fluid d-flex justify-content-center">
+        <span class="navbar-brand mb-0 h1 ">Hello World!</span>
+    </div>
+    </nav>
+    <!-- Navbar -->
+
+    <!-- Hero -->
+    <aside class="container d-flex justify-content-center hero-box">
+      <img class="profile" src="img/PasfotoAlmet.jpg" alt="">
+    
+      <img class="nextbuttn" src="img/nextbuttn.svg" alt="" onclick="hideart()">
+    </aside>
+
+    <main>
+      <article class="article textfont">
+        <p id="txtid">Hello, Im Muhammad Iqbal Qori Ayatillah known as Iqbal. <br><br>
+        Im a Undergraduate Informatics Engineering student at Institute <br>Technology Sepuluh Nopember Surabaya - Indonesia. <br><br>
+
+        Nice to meet you!, Im a student class of 2023 As a Computer Science <br>
+        enthusiast, I am dedicated to continual learning, Problem Solving, <br>
+        Visionary, Improving skills, relationships, and personal experiences. <br><br>
+
+        I have expertise in several skills, Such as Design Graphic, Video editing <br>
+        and Website development.
+
+      </p>
+
+    </article>
+
+    <article id="txtid2" class="d-none article textfont">
+    <p>
+      Yes this is my personal portfolio page. Unfortunately, it is still under <br> development,  so only provide a brief summary about myself. <br>
+    Thanks, See ya! <br><br>
+    </p>
+    <p class="socstxt">
+      Check My Socials! <br>
+    </p>
+    <p class="mt-4">
+    <img class="socslog" src="img/linkedin.svg" alt="">
+       : linkedin.com/in/muhammadiqbalqa <br>
+    </p>
+    <p>
+    <img class="socslog insta" src="img/instagram.svg" alt="">
+       : https://www.instagram.com/muhammdiqball/ <br>
+    </p>
+    </article>
+    </main>
+
+    
+
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+    <script src="js/script.js"></script>
+
+  </body>
+</html>
